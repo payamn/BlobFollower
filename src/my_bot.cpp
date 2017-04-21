3cbc7a610685c32649e91d2a72102992c6bf8dbd
Payam <payam222@gmail.com>
2017-04-21 16:15:07 -0700

    Follow from behind

--- src/my_bot.cpp
+++ src/my_bot.cpp
@@ -1,7 +1,4 @@
-#include <limits>
-#include <vector>
-#include <iostream>
-#include <cassert>
+#include <limits> include <vector> include <iostream> include <cassert>
 #include <algorithm>
 
 #include "my_bot.h"
@@ -13,20 +10,20 @@ static const double cruisespeed = 0.4;
 static const double avoidspeed = 0.05;
 static const double avoidturn = 0.5;
 static const double minfrontdistance = 1.0; // 0.6
-static const bool verbose = false;
 static const double stopdist = 0.3;
 static const int avoidduration = 10;
-
+static const int maxblobx = 79;
 typedef struct {
   ModelPosition *pos;
   ModelRanger *laser;
+  ModelBlobfinder *blob;
   int avoidcount, randcount;
 } robot_t;
 
+int BlobUpdate(Model *, robot_t *robot);
 int LaserUpdate(Model *mod, robot_t *robot);
-int PositionUpdate(Model *mod, robot_t *robot);
 
-// Stage calls this when the model starts up
+// Stage calls robotthis when the model starts up
 extern "C" int Init(Model *mod, CtrlArgs *)
 {
   // local arguments
@@ -48,9 +45,7 @@ extern "C" int Init(Model *mod, CtrlArgs *)
     exit(1);
   }
 
-  if (verbose)
-    robot->pos->AddCallback(Model::CB_UPDATE, model_callback_t(PositionUpdate), robot);
-
+  
   robot->pos->Subscribe(); // starts the position updates
 
   ModelRanger *first_laser = dynamic_cast<ModelRanger *>(robot->pos->GetChild("ranger:0")),
@@ -65,12 +60,40 @@ extern "C" int Init(Model *mod, CtrlArgs *)
   robot->laser->AddCallback(Model::CB_UPDATE, model_callback_t(LaserUpdate), robot);
   robot->laser->Subscribe(); // starts the ranger updates
 
+  robot->blob = dynamic_cast<ModelBlobfinder *>(robot->pos->GetChild("blobfinder:0"));//(robot->pos->GetWorld()->GetModel("blob"));
+  if (!robot->blob)
+    std::cout << "error" << "\n";
+  robot->blob->AddCallback(Model::CB_UPDATE, model_callback_t(BlobUpdate), robot);
+  robot->blob->Subscribe();
   return 0; // ok
 }
 
+int BlobUpdate(Model *, robot_t *robot)
+{
+  // no blob
+  if (!robot->blob->GetBlobs().size())
+    return 0;
+  int centerPointX = (robot->blob->GetBlobs()[0].left + robot->blob->GetBlobs()[0].right) /2;
+  std::cout << centerPointX <<  " " << robot->blob->GetBlobs()[0].range<< std::endl;
+  
+  if (centerPointX < maxblobx /2 -1){ 
+    std::cout << "left" << std::endl;
+    robot->pos->SetTurnSpeed(avoidturn);
+  }
+  else if (centerPointX > maxblobx /2 + 1){ 
+    std::cout << "right" << std::endl;
+    robot->pos->SetTurnSpeed(-avoidturn);
+  }
+  else{
+    robot->pos->SetTurnSpeed(0);
+  }
+
+  return 0; // run again
+}
 // inspect the ranger data and decide what to do
 int LaserUpdate(Model *, robot_t *robot)
 {
+
   // get the data
   const std::vector<meters_t> &scan = robot->laser->GetSensors()[0].ranges;
   uint32_t sample_count = scan.size();
@@ -86,19 +109,13 @@ int LaserUpdate(Model *, robot_t *robot)
   double minright = 1e6;
 
   for (uint32_t i = 0; i < sample_count; i++) {
-    if (verbose)
-      printf("%.3f ", scan[i]);
 
     if ((i > (sample_count / 3)) && (i < (sample_count - (sample_count / 3)))
         && scan[i] < minfrontdistance) {
-      if (verbose)
-        puts("  obstruction!");
       obstruction = true;
     }
 
     if (scan[i] < stopdist) {
-      if (verbose)
-        puts("  stopping!");
       stop = true;
     }
 
@@ -108,44 +125,28 @@ int LaserUpdate(Model *, robot_t *robot)
       minright = std::min(minright, scan[i]);
   }
 
-  if (verbose) {
-    puts("");
-    printf("minleft %.3f \n", minleft);
-    printf("minright %.3f\n ", minright);
-  }
-
+ 
   if (obstruction || stop || (robot->avoidcount > 0)) {
-    if (verbose)
-      printf("Avoid %d\n", robot->avoidcount);
-
     robot->pos->SetXSpeed(stop ? 0.0 : avoidspeed);
 
     /* once we start avoiding, select a turn direction and stick
  with it for a few iterations */
     if (robot->avoidcount < 1) {
-      if (verbose)
-        puts("Avoid START");
       robot->avoidcount = random() % avoidduration + avoidduration;
 
       if (minleft < minright) {
         robot->pos->SetTurnSpeed(-avoidturn);
-        if (verbose)
-          printf("turning right %.2f\n", -avoidturn);
       } else {
         robot->pos->SetTurnSpeed(+avoidturn);
-        if (verbose)
-          printf("turning left %2f\n", +avoidturn);
       }
     }
 
     robot->avoidcount--;
   } else {
-    if (verbose)
-      puts("Cruise");
 
     robot->avoidcount = 0;
     robot->pos->SetXSpeed(cruisespeed);
-    robot->pos->SetTurnSpeed(0);
+    //robot->pos->SetTurnSpeed(0);
   }
 
   //  if( robot->pos->Stalled() )
@@ -157,11 +158,3 @@ int LaserUpdate(Model *, robot_t *robot)
   return 0; // run again
 }
 
-int PositionUpdate(Model *, robot_t *robot)
-{
-  Pose pose = robot->pos->GetPose();
-
-  printf("Pose: [%.2f %.2f %.2f %.2f]\n", pose.x, pose.y, pose.z, pose.a);
-
-  return 0; // run again
-}
