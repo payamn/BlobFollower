28024fd0cc2c91f0f7508257019134f977c8c6f7
payam nikdel <payam222@gmail.com>
2017-05-17 17:43:28 -0700

    working using laser scanner

--- src/my_bot.cpp
+++ src/my_bot.cpp
@@ -9,7 +9,8 @@
 
 
 using namespace Stg;
-static const bool UseOtherRobotPose = true;
+static const bool UseOtherRobotPose = false;
+static const bool UseLaser = true;
 static const double followdist = 3;
 static const double cruisespeed = 0.4;
 static const double avoidspeed = 0.05;
@@ -17,17 +18,17 @@ static const double avoidturn = 0.5;
 static const double minfrontdistance = 1.0; // 0.6
 static const double stopdist = 0.3;
 static const int avoidduration = 10;
+static const int numberOfPosHist = 10;
 int maxblobx = 80;
 static bool isahead = true;
 typedef struct {
   ModelPosition *pos;
   ModelRanger *laser;
   ModelBlobfinder *blob;
-  Stg::Pose lastposeother0;
-  Stg::Pose lastposeother1;
-  Stg::Pose lastposeother2;
-  Stg::Pose lastposeother3;
-  Stg::Pose lastDestination;
+  Stg::Pose posesAvgLastOther;
+  Stg::Pose* posesOther;
+  int TheposesOtherNum;
+  Stg::Pose* lastDestination;
   int state; // 1 means blob detected, 0 not detected
   PID pidcruse;
   PID pidturn;
@@ -41,6 +42,7 @@ typedef struct {
 int counter = 0;
 int BlobUpdate(Model *, robot_t *robot);
 int LaserUpdate(Model *mod, robot_t *robot);
+int setSpeed(robot_t *robot, Stg::Pose destination);
 
 // Stage calls robotthis when the model starts up
 extern "C" int Init(Model *mod, CtrlArgs *)
@@ -58,18 +60,17 @@ extern "C" int Init(Model *mod, CtrlArgs *)
   // robot->pidcruse = PID(cruisespeed, -cruisespeed, 0.1, 0.01, 0.5);
   if (isahead){
     robot->pidturn.set(avoidturn*2, -avoidturn*2, 0.84, 0.041, 0.1);
-    robot->pidcruse.set(0.,cruisespeed*2, cruisespeed*2  , 1.0001, 0.0);
+    robot->pidcruse.set(cruisespeed*4, -cruisespeed*2, -cruisespeed  , .0000, 0.000);
   }
-  else {
-    robot->pidturn.set(avoidturn, -avoidturn, 0.04, 0.0005, 0.000);
-    robot->pidcruse.set(0, cruisespeed, cruisespeed*4, 0.001, 0.001);
-  }
+  // else {
+  //   robot->pidturn.set(avoidturn, -avoidturn, 0.04, 0.0005, 0.000);
+  //   robot->pidcruse.set(0, cruisespeed, cruisespeed*4, 1.001, 0.001);
+  // }
   robot->avoidcount = 0;
-  robot->lastposeother0  = Stg::Pose(0.,0.,0.,0.);
-  robot->lastposeother1 = Stg::Pose(0.,0.,0.,0.);
-  robot->lastposeother2  = Stg::Pose(0.,0.,0.,0.);
-  robot->lastposeother3  = Stg::Pose(0.,0.,0.,0.);
-  robot->lastDestination  = Stg::Pose(0.,0.,0.,0.);
+  robot->posesAvgLastOther  = Stg::Pose(0.,0.,0.,0.);
+  robot->posesOther  = new Stg::Pose[numberOfPosHist];
+  robot->TheposesOtherNum = 0;
+  robot->lastDestination  = new Stg::Pose[numberOfPosHist];
   robot->randcount = 0;
   robot->distance = 0;
   robot->state = 0;
@@ -90,17 +91,11 @@ extern "C" int Init(Model *mod, CtrlArgs *)
   // }
   robot->pos->Subscribe(); // starts the position updates
 
-  ModelRanger *first_laser = dynamic_cast<ModelRanger *>(robot->pos->GetChild("ranger:0")),
-              *second_laser = dynamic_cast<ModelRanger *>(mod->GetChild("ranger:1"));
-  if (!first_laser || !second_laser) {
-    PRINT_ERR1("Wander controller requires 2 range sensors (%u given).",
-               unsigned(bool(first_laser) + bool(second_laser)));
-    exit(2);
-  }
-  first_laser->Subscribe();
-  robot->laser = second_laser;
-  robot->laser->AddCallback(Model::CB_UPDATE, model_callback_t(LaserUpdate), robot);
-  robot->laser->Subscribe(); // starts the ranger updates
+
+  robot->laser = (ModelRanger*)mod->GetChild("ranger:1");
+  robot->laser->AddCallback( Model::CB_UPDATE, model_callback_t(LaserUpdate), robot);
+  robot->laser->Subscribe();
+
 
   robot->blob = dynamic_cast<ModelBlobfinder *>(robot->pos->GetChild("blobfinder:0"));//(robot->pos->GetWorld()->GetModel("blob"));
   if (!robot->blob)
@@ -112,6 +107,8 @@ extern "C" int Init(Model *mod, CtrlArgs *)
 
 int BlobUpdate(Model * mod, robot_t *robot)
 {
+  if (UseLaser)
+    return 0;
   double speedX = 0;
   double speedY = 0;
   // no blob
@@ -124,25 +121,22 @@ int BlobUpdate(Model * mod, robot_t *robot)
 
     double x = robot->otherRobot->GetGlobalPose().x;
     double y = robot->otherRobot->GetGlobalPose().y;
-    speedX = x - robot->lastposeother0.x; 
-    speedY = y - robot->lastposeother0.y;          
+    speedX = x - robot->posesOther[0].x; 
+    speedY = y - robot->posesOther[0].y;          
     
-    robot->lastposeother0 = Stg::Pose(x,y,speedX,speedY);
+    robot->posesOther[0] = Stg::Pose(x,y,speedX,speedY);
 
     double speedXU = speedX / sqrt(speedX*speedX + speedY*speedY);
     double speedYU = speedY / sqrt(speedX*speedX + speedY*speedY);
-    robot->lastDestination = Stg::Pose(x+speedX*2 + speedXU*followdist, y+speedY*2 + speedYU*followdist, 0.,0.);
-    ModelPosition::Waypoint wp(robot->lastDestination,
+    robot->lastDestination[0] = Stg::Pose(x+speedX*2 + speedXU*followdist, y+speedY*2 + speedYU*followdist, 0.,0.);
+    ModelPosition::Waypoint wp(robot->lastDestination[0],
                                              Color("blue"));
-    ((ModelPosition*)robot->otherRobot)-> waypoints.push_back(wp);
-    std::cout << "speed x y a: " << speedX << " " << speedY << " "<< std::endl 
-    << " destination pos: " << robot->lastDestination.x << " " << robot->lastDestination.y << std::endl;
+
   }
   else if (!robot->blob->GetBlobs().size() ){
-    robot->state = 0;
-    speedX = robot->lastposeother0.z;
-    speedY = robot->lastposeother0.a;
-    robot->lastDestination = Stg::Pose(robot->lastDestination.x+speedX,robot->lastDestination.y+speedY,0,0);
+    speedX = robot->posesOther[0].z;
+    speedY = robot->posesOther[0].a;
+    robot->lastDestination[0] = Stg::Pose(robot->lastDestination[0].x+speedX,robot->lastDestination[0].y+speedY,0,0);
 
   }
   else{
@@ -150,7 +144,6 @@ int BlobUpdate(Model * mod, robot_t *robot)
     double f = maxblobx / tan(robot->blob->fov/2)/2;
     
     std::cout << "Focal: " << f << std::endl;
-    robot->state = 1;
     double range = robot->blob->GetBlobs()[0].range;
     int centerPointX = (robot->blob->GetBlobs()[0].left + robot->blob->GetBlobs()[0].right) /2;
     int centerPointY = (robot->blob->GetBlobs()[0].top + robot->blob->GetBlobs()[0].bottom) /2;
@@ -249,27 +242,27 @@ int BlobUpdate(Model * mod, robot_t *robot)
         std::cout << "counter: " << counter%4 << std::endl;
         counter++;
     switch(counter%4){
-      case 0:{ robot->lastposeother0.x = x; robot->lastposeother0.y=y;
-ModelPosition::Waypoint wp(Stg::Pose(x,y,robot->otherRobot->GetGlobalPose().z,0.),
+      case 0:{ robot->posesOther[0].x = x; robot->posesOther[0].y=y;
+    ModelPosition::Waypoint wp(Stg::Pose(x,y,robot->otherRobot->GetGlobalPose().z,0.),
                                              Color("red"));
     ((ModelPosition*)robot->otherRobot)-> waypoints.push_back(wp);
               return 0;
-}
-      case 1: {robot->lastposeother0.x = (robot->lastposeother0.x + x)/2; 
-                    robot->lastposeother0.y = (robot->lastposeother0.y + y)/2;
+      } 
+      case 1: {robot->posesOther[0].x = (robot->posesOther[0].x + x)/2; 
+                    robot->posesOther[0].y = (robot->posesOther[0].y + y)/2;
       ModelPosition::Waypoint wp(Stg::Pose(x,y,robot->otherRobot->GetGlobalPose().z,0.),
                                                    Color("red"));
           ((ModelPosition*)robot->otherRobot)-> waypoints.push_back(wp);
                     return 0;
             }
-      case 2: {robot->lastposeother1.x = x; robot->lastposeother1.y=y;
+      case 2: {robot->posesOther[1].x = x; robot->posesOther[1].y=y;
       ModelPosition::Waypoint wp(Stg::Pose(x,y,robot->otherRobot->GetGlobalPose().z,0.),
                                                    Color("red"));
           ((ModelPosition*)robot->otherRobot)-> waypoints.push_back(wp);
                     return 0;
             }
-      case 3: {x = (robot->lastposeother1.x + x)/2; 
-                    y = (robot->lastposeother1.y + y)/2;
+      case 3: {x = (robot->posesOther[1].x + x)/2; 
+                    y = (robot->posesOther[1].y + y)/2;
                     break;}
     }
 
@@ -277,37 +270,172 @@ ModelPosition::Waypoint wp(Stg::Pose(x,y,robot->otherRobot->GetGlobalPose().z,0.
                                              Color("green"));
     ((ModelPosition*)robot->otherRobot)-> waypoints.push_back(wp);
 
-    speedX = x - robot->lastposeother0.x; 
-    speedY = y - robot->lastposeother0.y;          
+    speedX = x - robot->posesOther[0].x; 
+    speedY = y - robot->posesOther[0].y;          
     if (speedX == 0 && speedY == 0){
-      speedX = robot->lastposeother0.z;
-      speedY = robot->lastposeother0.a;
+      speedX = robot->posesOther[0].z;
+      speedY = robot->posesOther[0].a;
     }
 
     double speedXU = speedX / sqrt(speedX*speedX + speedY*speedY);
     double speedYU = speedY / sqrt(speedX*speedX + speedY*speedY);
-    robot->lastDestination = Stg::Pose(x+speedX*2 + speedXU*followdist, y+speedY*2 + speedYU*followdist, 0.,0.);
-    robot->lastposeother0 = Stg::Pose(x,y,speedX,speedY);          
+    robot->lastDestination[0] = Stg::Pose(x+speedX*2 + speedXU*followdist, y+speedY*2 + speedYU*followdist, 0.,0.);
+    robot->posesOther[0] = Stg::Pose(x,y,speedX,speedY);          
 
-    ModelPosition::Waypoint wp1(robot->lastDestination,
+    ModelPosition::Waypoint wp1(robot->lastDestination[0],
                                              Color("blue"));
     ((ModelPosition*)robot->otherRobot)-> waypoints.push_back(wp1);
     std::cout <<  maxblobx /2 - centerPointX << " range:" << range <<"\n" 
           // <<  robot->blob->GetBlobs()[0].left<< " right:" 
           // << robot->blob->GetBlobs()[0].right  <<  " " 
           // << robot->blob->GetBlobs()[0].range << " "  
-          << "pose dest: " << robot->lastDestination.x << " " << robot->lastDestination.y << "\n"               
+          << "pose dest: " << robot->lastDestination[0].x << " " << robot->lastDestination[0].y << "\n"               
           << "pose secn: " << x << " " << y << " rotation:" << robot->pos->GetPose().a * 180 / M_PI  << "\n" 
           << "speed secn: " << speedX << " " << speedY << "\n" 
           << std::endl;
     }
 
-  double m = (robot->lastDestination.y - robot->pos->GetPose().y) / (robot->lastDestination.x - robot->pos->GetPose().x);
+  setSpeed(robot,robot->lastDestination[0]);
+
+  return 0; // run again
+}
+ 
+
+ int getOtherRobotPose(const Stg::ModelRanger::Sensor& sensor,  robot_t *robot, Stg::Pose &otherRobot)
+{
+    // get the data
+    const std::vector<Stg::meters_t>& scan = sensor.ranges;
+  
+    uint32_t sample_count = scan.size();
+    if( sample_count < 1 )
+        return 1;
+
+    double laser_orientation =  robot->pos->GetPose().a - sensor.fov/2.0;
+    double angle_increment = sensor.fov/(double)(sensor.sample_count-1);
+    double other_x_avg = 0.0;
+    double other_y_avg = 0.0;
+    double num_point = 0.0;
+    for (uint32_t i = 0; i < sample_count; i++) {
+        // normalize the angle
+        laser_orientation = atan2(sin(laser_orientation), cos(laser_orientation));
+        
+        double laser_x, laser_y;
+        int laser_grid_x, laser_grid_y;
+
+        laser_x =  robot->pos->GetPose().x +  scan[i] * cos(laser_orientation);
+        laser_y =  robot->pos->GetPose().y +  scan[i] * sin(laser_orientation);
+        
+        // if (convertToGridCoords(laser_x, laser_y, laser_grid_x, laser_grid_y)) {
+        //     continue;
+        // }
+
+
+        if ( scan[i] < (sensor.range.max - std::numeric_limits<float>::min()) ) {
+          num_point++;
+          other_x_avg += laser_x;
+          other_y_avg += laser_y;
+        }
+
+        laser_orientation += angle_increment;
+    }
+    std::cout << "number point: " << num_point << std::endl;
+    if (num_point){
+      std::cout << "get other robot pos xy: "<< other_x_avg/num_point << " " << other_y_avg/num_point << std::endl;
+      otherRobot = Stg::Pose(other_x_avg/num_point,other_y_avg/num_point,robot->otherRobot->GetGlobalPose().z,0.);
+      ModelPosition::Waypoint wp(Stg::Pose(other_x_avg/num_point,other_y_avg/num_point,robot->otherRobot->GetGlobalPose().z,0.), Color("green"));
+      ((ModelPosition*)robot->otherRobot)-> waypoints.push_back(wp);
+    }
+    else {
+     return 1;
+    }
+    
+
+    return 0;
+}
+
+// inspect the ranger data and decide what to do
+bool firstTime = true;
+int laserCounter =0;
+int LaserUpdate(Model *, robot_t *robot)
+{
+  if (!UseLaser)
+    return 0;
+
+
+  int posNum = robot->TheposesOtherNum % numberOfPosHist;
+  Stg::Pose otherRobot;
+  if (getOtherRobotPose (robot->laser->GetSensors()[0], robot, otherRobot)) // no position found
+    return 0;
+  double xTemp = otherRobot.x;
+  double yTemp =  otherRobot.y;
+  if (firstTime){
+    robot->posesAvgLastOther = Stg::Pose(xTemp,yTemp,0,0);
+    for (int i=0; i<numberOfPosHist ; i++){
+       robot->posesOther[i] = Stg::Pose(xTemp,yTemp,0,0);
+       robot->lastDestination[i] = Stg::Pose(xTemp,yTemp,0,0);
+       std::cout << "XY temp: " << xTemp << " " << yTemp << " "<< std::endl;
+    }
+    firstTime =false;
+  }
+  robot->posesOther[posNum] = Stg::Pose(xTemp,yTemp,0,0);
+
+  //-----------------------gitting pos history average-------------------------------
+  double sumX = 0.0;
+  double sumY = 0.0;
+  for (int i=0; i<numberOfPosHist ; i++){
+    sumX += robot->posesOther[i].x;
+    sumY += robot->posesOther[i].y; 
+  }
+
+  double x = sumX/(double)numberOfPosHist;
+  double y = sumY/(double)numberOfPosHist;
+
+  double speedX = x - robot->posesAvgLastOther.x; 
+  double speedY = y - robot->posesAvgLastOther.y;        
+   if (speedX == 0 && speedY == 0){
+    speedX = robot->posesAvgLastOther.z;
+    speedY = robot->posesAvgLastOther.a;
+  }
+  robot->posesAvgLastOther = Stg::Pose(x,y,speedX,speedY);
+  robot->posesOther[posNum] = Stg::Pose(xTemp,yTemp,0,0);
+  if (speedX*speedX + speedY*speedY == 0){   //to avoid devide by 0
+    return 0;
+  }
+  double speedXU = speedX / sqrt(speedX*speedX + speedY*speedY);
+  double speedYU = speedY / sqrt(speedX*speedX + speedY*speedY);
+  robot->lastDestination[posNum] = Stg::Pose(x+speedX*2 + speedXU*followdist, y+speedY*2 + speedYU*followdist, 0.,0.);
+  //-----------------------gitting dest history average focus on last destination---------
+  Stg::Pose avgDestinations= Stg::Pose(0,0,0,0);
+  double devide = 1;
+  for (int i=0 ; i<numberOfPosHist-1 ; i++){
+    devide *= 0.5;
+    avgDestinations.x += robot->lastDestination[(posNum-i+ numberOfPosHist)%numberOfPosHist].x * devide;
+    avgDestinations.y += robot->lastDestination[(posNum-i + numberOfPosHist)%numberOfPosHist].y * devide;
+    std::cout << (posNum-i+ numberOfPosHist)%numberOfPosHist  <<std::endl;
+  }
+  std::cout <<  (posNum+ numberOfPosHist-(numberOfPosHist-1))  <<std::endl;
+ 
+  avgDestinations.x += robot->lastDestination[(posNum+numberOfPosHist-(numberOfPosHist-1))%numberOfPosHist].x * devide;
+  avgDestinations.y += robot->lastDestination[(posNum + numberOfPosHist -(numberOfPosHist-1))%numberOfPosHist].y * devide;
+
+  setSpeed(robot, avgDestinations);
+  robot->TheposesOtherNum ++;
+
+  return 0;
+ }
+
+
+int setSpeed(robot_t *robot, Stg::Pose destination){
+
+  ModelPosition::Waypoint wp(destination ,Color("blue"));
+  ((ModelPosition*)robot->otherRobot)-> waypoints.push_back(wp);
+
+  double m = (destination.y - robot->pos->GetPose().y) / (destination.x - robot->pos->GetPose().x);
   double degreeD = atan(m);
 
-  if (robot->lastDestination.x < robot->pos->GetPose().x && robot->lastDestination.y > robot->pos->GetPose().y  )
+  if (destination.x < robot->pos->GetPose().x && destination.y > robot->pos->GetPose().y  )
     degreeD = M_PI + degreeD;  
-  else if (robot->lastDestination.x < robot->pos->GetPose().x && robot->lastDestination.y < robot->pos->GetPose().y  )
+  else if (destination.x < robot->pos->GetPose().x && destination.y < robot->pos->GetPose().y  )
     degreeD = -M_PI + degreeD;
 
   if (abs(degreeD -robot->degreeD) >= 180){
@@ -316,109 +444,18 @@ ModelPosition::Waypoint wp(Stg::Pose(x,y,robot->otherRobot->GetGlobalPose().z,0.
   }
   robot->degreeD = degreeD;
   std::cout << "m: "<< m << "atan: " <<  degreeD*180 / M_PI << std::endl;
-  //set turn speed']
+ 
+  //set turn speed
   robot->pos->SetTurnSpeed(
     robot->pidturn.calculate( degreeD , robot->pos->GetPose().a  , 0.01 )
   );
 
-  // robot->lastposeother = centerPointX ;
-  // if (centerPointX < maxblobx /2 ){ 
-  //   std::cout << "left" << std::endl;
-  //   robot->pos->SetTurnSpeed((avoidtursn/10.0)*(maxblobx /2 - centerPointX));
-  // }
-  // else if (centerPointX > maxblobx /2 ){ 
-  //   std::cout << "right" << std::endl;
-  //   robot->pos->SetTurnSpeed(-(avoidturn/10.0)*(centerPointX-maxblobx /2 ));
-  // }
-  // else{
-  //   robot->pos->SetTurnSpeed(0);
-  // }
-  // set forward speed
-  // if (robot->blob->GetBlobs()[0].range >  followdist){
 
-    robot->distance = sqrt(pow(robot->lastDestination.x- robot->pos->GetPose().x, 2) +  pow(robot->lastDestination.y - robot->pos->GetPose().y, 2));
-    std::cout<< "distance: " << robot->distance << std::endl;
-    robot->pos->SetXSpeed(
-    robot->pidcruse.calculate(0 ,robot->distance, 0.01 )
-    );
-  
-
-    // robot->pos->SetXSpeed((cruisespeed*4)*( robot->blob->GetBlobs()[0].range) - followdist);
-  // }                                    
-  // else{
-  //   std::cout << "speed 0" << std::endl;
-  //   robot->pos->SetXSpeed(0);
-  // }
+  robot->distance = sqrt(pow(destination.x- robot->pos->GetPose().x, 2) +  pow(destination.y - robot->pos->GetPose().y, 2));
+  std::cout<< "distance: " << robot->distance << std::endl;
+  double Xspeed = robot->pidcruse.calculate(0 ,robot->distance, 0.01 );
+  std::cout << "Xspeed: " << Xspeed << std::endl;
+  robot->pos->SetXSpeed(Xspeed);
 
   return 0; // run again
 }
-// inspect the ranger data and decide what to do
-int LaserUpdate(Model *, robot_t *robot)
-{
-  return 0;
-  if (robot->state)
-    return 0;
-  // get the data
-  const std::vector<meters_t> &scan = robot->laser->GetSensors()[0].ranges;
-  uint32_t sample_count = scan.size();
-  if (sample_count < 1)
-    return 0;
-
-  bool obstruction = false;
-  bool stop = false;
-
-  // find the closest distance to the left and right and check if
-  // there's anything in front
-  double minleft = 1e6;
-  double minright = 1e6;
-
-  for (uint32_t i = 0; i < sample_count; i++) {
-
-    if ((i > (sample_count / 3)) && (i < (sample_count - (sample_count / 3)))
-        && scan[i] < minfrontdistance) {
-      obstruction = true;
-    }
-
-    if (scan[i] < stopdist) {
-      stop = true;
-    }
-
-    if (i > sample_count / 2)
-      minleft = std::min(minleft, scan[i]);
-    else
-      minright = std::min(minright, scan[i]);
-  }
-
- 
-  if (obstruction || stop || (robot->avoidcount > 0)) {
-    robot->pos->SetXSpeed(stop ? 0.0 : avoidspeed);
-
-    /* once we start avoiding, select a turn direction and stick
- with it for a few iterations */
-    if (robot->avoidcount < 1) {
-      robot->avoidcount = random() % avoidduration + avoidduration;
-
-      if (minleft < minright) {
-        robot->pos->SetTurnSpeed(-avoidturn);
-      } else {
-        robot->pos->SetTurnSpeed(+avoidturn);
-      }
-    }
-
-    robot->avoidcount--;
-  } else {
-
-    robot->avoidcount = 0;
-    robot->pos->SetXSpeed(cruisespeed);
-    //robot->pos->SetTurnSpeed(0);
-  }
-
-  //  if( robot->pos->Stalled() )
-  //     {
-  //        robot->pos->SetSpeed( 0,0,0 );
-  //        robot->pos->SetTurnSpeed( 0 );
-  // }
-
-  return 0; // run again
-}
-
